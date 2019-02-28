062ce756669875e853f36a630569573378119c15
solone313 <wjstk1233@gmail.com>
2019-02-28 21:40:36 +0900

    ~

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '*k7uq1w)z)lriw7ogrd+mgd-hc0+6gh7_w#o*9vefo^ils(=35'
-import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
@@ -123,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_mine'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
